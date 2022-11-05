00e839ca9893692f5f0487961708aae1fde81f45
Tim Hermens <tim.hermens@rabobank.nl>
2022-11-05 21:59:45 +0100

    Revert "chore: prettier js"
    This reverts commit 289e5576668972a4b90f676983438d392d4fbf66.

--- tools/schematics/src/utils/changes.js
+++ tools/schematics/src/utils/changes.js
@@ -1,25 +1,25 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.ReplaceChange = void 0;
 class ReplaceChange {
-  constructor(path, pos, end, newText) {
-    this.path = path;
-    this.pos = pos;
-    this.end = end;
-    this.newText = newText;
-    if (pos < 0) {
-      throw new Error('Negative positions are invalid');
+    constructor(path, pos, end, newText) {
+        this.path = path;
+        this.pos = pos;
+        this.end = end;
+        this.newText = newText;
+        if (pos < 0) {
+            throw new Error('Negative positions are invalid');
+        }
+        this.description = `Replaced position '${pos} to ${end}' with ${newText}`;
+        this.order = pos;
     }
-    this.description = `Replaced position '${pos} to ${end}' with ${newText}`;
-    this.order = pos;
-  }
-  apply(host) {
-    return host.read(this.path).then((content) => {
-      const prefix = content.substring(0, this.pos);
-      const suffix = content.substring(this.pos + this.end);
-      return host.write(this.path, `${prefix}${this.newText}${suffix}`);
-    });
-  }
+    apply(host) {
+        return host.read(this.path).then(content => {
+            const prefix = content.substring(0, this.pos);
+            const suffix = content.substring(this.pos + this.end);
+            return host.write(this.path, `${prefix}${this.newText}${suffix}`);
+        });
+    }
 }
 exports.ReplaceChange = ReplaceChange;
 //# sourceMappingURL=changes.js.map
