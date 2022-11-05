a343507fb85a5a30c4d21c445b6a8f5118127206
Tim Hermens <tim.hermens@rabobank.nl>
2022-11-05 22:30:21 +0100

    chore: added flight edit feature

---
+++ libs/flight/features/flight-edit/README.md
@@ -0,0 +1,7 @@
+# flight-features-flight-edit
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test flight-features-flight-edit` to execute the unit tests.
---
+++ libs/flight/features/flight-edit/src/lib/flight-features-flight-edit.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlightEditComponent } from './flight-edit/flight-edit.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [FlightEditComponent],
+})
+export class FlightFeaturesFlightEditModule {}
