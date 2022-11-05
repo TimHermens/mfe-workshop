2fd2df5c8149476299f5439b0770e633a12046b8
Tim Hermens <tim.hermens@rabobank.nl>
2022-11-05 17:37:11 +0100

    chore: test

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { LuggageUtilModule } from '@flight-workspace/luggage-util';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-}
+export class AppComponent {}
