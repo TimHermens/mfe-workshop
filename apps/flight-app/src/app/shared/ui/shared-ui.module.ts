7c7d1ffe3c6476c642acbdd6fb93858c5932058f
Tim Hermens <tim.hermens@rabobank.nl>
2022-11-06 10:57:38 +0100

    chore: deleted unused shared module

--- apps/flight-app/src/app/shared/ui/shared-ui.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { BreadcrumbModule } from './breadcrumb/breadcrumb.module';
-
-@NgModule({
-  imports: [CommonModule, BreadcrumbModule],
-  exports: [BreadcrumbModule],
-})
-export class SharedUiModule {}
