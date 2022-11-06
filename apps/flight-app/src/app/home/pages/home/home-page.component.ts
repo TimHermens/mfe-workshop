76a4b33abc12078b26d3b737e079755e8ddb83ec
Tim Hermens <tim.hermens@rabobank.nl>
2022-11-06 01:22:45 +0100

    chore: removed all tightly coupled code

--- apps/flight-app/src/app/home/pages/home/home-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'page-home',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'],
-})
-export class HomePageComponent {}
--- apps/flight-app/src/app/passengers/models/passenger.ts
+++
@@ -1,7 +0,0 @@
-export interface Passenger {
-  id: number;
-  name: string;
-  firstName: string;
-  bonusMiles: number;
-  passengerStatus: string;
-}
